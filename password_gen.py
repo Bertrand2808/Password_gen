9898fe3a3977da71a1d76882319974c9f6a4e58e
Bertrand RENAUDIN <bertrand.renaudin@odigo.com>
2022-11-02 12:53:35 +0100

    écriture dans un fichier txt en append

--- password_gen.py
+++ password_gen.py
@@ -3,16 +3,13 @@ import random
 print('Bienvenue dans votre générateur de mot de passe.')
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*().,?'
-
 site = input('Saisissez le site pour lequel vous voulez enregistrer le mot de passe : ') 
-
 mail = input('Saisissez votre adresse mail : ')
-
-# number = input('Saisissez le nombre de mot de passe à générer : ')
-# number = int(number)
-number = 1
+number = 1 # nombre de mot de passe à générer
 length = input('Entrez la longueur du mot de passe : ')
 length = int(length)
+
+fichier = open("passwords.txt", "a")
 
 print('\nVoici votre mot de passe pour le site : ' + site)
 
@@ -21,4 +18,6 @@ for pwd in range(number):
     for c in range(length):
         passwords += random.choice(chars)
     print(passwords)
+    fichier.write('\n' + site + '  :  ' + mail + '  :  ' + passwords)
+    fichier.close()
     
